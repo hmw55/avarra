0590fd57bea600a3e1399748460f698cb1982bb7
Mack <hollandwesley55@gmail.com>
2026-09-09 13:52:08 -0600

    chore: remove unused test import

--- backend/src/test/java/dev/hollandwesley/avarra/user/RegisterUserValidationTests.java
+++ backend/src/test/java/dev/hollandwesley/avarra/user/RegisterUserValidationTests.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class RegisterUserRequestValidationTests {
 
