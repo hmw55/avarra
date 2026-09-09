344470c9ecd6d406d8756374b4f975dde98b25d2
Holland M. Wesley <hollandwesley55@gmail.com>
2026-09-09 01:26:46 -0600

    chore: bootstrap Spring Boot backend

---
+++ backend/src/main/java/dev/hollandwesley/avarra/AvarraApplication.java
@@ -0,0 +1,13 @@
+package dev.hollandwesley.avarra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvarraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvarraApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/dev/hollandwesley/avarra/AvarraApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.hollandwesley.avarra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvarraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
